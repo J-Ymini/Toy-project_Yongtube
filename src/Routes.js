4020e82082377f2ded2a51ee3ccc259f3a70f3f3
yongminJeon <dydalsdl1414@gmail.com>
2021-08-05 16:58:50 +0900

    misc: master branch 초기세팅

---
+++ src/Routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+export default function Routes() {
+  return (
+    <Router>
+      <Switch>
+        <Route />
+      </Switch>
+    </Router>
+  );
+}
