179eb622db5b2473502aafd9f014a66af3f08840
yongminJeon <dydalsdl1414@gmail.com>
2021-08-07 10:10:08 +0900

    feat: fetch 로직 async로 변경

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -7,24 +7,22 @@ class Youtube {
     };
   }
 
-  mostPopular() {
-    return fetch(
+  async mostPopular() {
+    const response = await fetch(
       `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
       this.getRequestOptions
-    )
-      .then(response => response.json())
-      .then(({ items }) => items);
+    );
+    const { items } = await response.json();
+    return items;
   }
 
-  search(query) {
-    return fetch(
+  async search(query) {
+    const response = await fetch(
       `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
       this.getRequestOptions
-    )
-      .then(response => response.json())
-      .then(result => {
-        return result.items.map(item => ({ ...item, id: item.id.videoId }));
-      });
+    );
+    const result = await response.json();
+    return result.items.map(item => ({ ...item, id: item.id.videoId }));
   }
 }
 
